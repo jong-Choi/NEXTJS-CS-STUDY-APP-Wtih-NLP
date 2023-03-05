ba1b0bf85de7ee3afc30c53dff8bcb7020a090dc
bluecoolgod80@gmail.com <bluecoolgod80@gmail.com>
2023-03-06 00:51:24 +0900

    PWA 배포

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
+const nextConfig = withPWA({
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
-};
+});
 
 module.exports = nextConfig;
