38eda5cb59b6cc23a6365254ca9742b9de718d3c
bluecoolgod80@gmail.com <bluecoolgod80@gmail.com>
2023-03-06 03:09:29 +0900

    Head 위치 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { ThemeProvider, DefaultTheme } from "styled-components";
 import GlobalStyle from "../components/globalstyles";
 
@@ -12,6 +13,31 @@ const theme: DefaultTheme = {
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>CS 문제 풀기</title>
+        <meta name="description" content="CS 문제를 자유롭게 풀어봐요" />
+        <link rel="icon" href="/favicon.ico" />
+        {/* <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no"
+          /> */}
+        <meta name="application-name" content="CS 퀴즈풀기" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="CS 퀴즈풀기" />
+        <meta name="description" content="CS 문제를 마음대로 풀어보세요" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#000000" />
+
+        <link rel="apple-touch-icon" href="/favicon.jpg" />
+
+        <link rel="mask-icon" href="/favicon.jpg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
